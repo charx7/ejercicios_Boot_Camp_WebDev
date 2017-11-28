a7c58a7e24cc5125dc000b3f4fda3175f7c43848
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-11-28 12:58:28 -0600

    Despliegue del mensaje intenta otra vez XD

--- Ejercicio_Dom_Manipulation/GuessColorGame/funcionalidadColores.js
+++ Ejercicio_Dom_Manipulation/GuessColorGame/funcionalidadColores.js
@@ -17,6 +17,8 @@ var colorGanador = colores[3];
 var colorDesplegado = document.getElementById("desplegadoColor");
 colorDesplegado.textContent = colorGanador;
 
+// Seleccionamos span de texto que indicara al usuario si escogieron bien el color o no
+var mensajeDesplegar = document.querySelector('#mensaje');
 
 // Cambiamos el color de cada cuadrado de acuerdo al color del arreglo de colores
 for(var i = 0; i<arregloCuadrados.length; i++) {
@@ -25,9 +27,15 @@ for(var i = 0; i<arregloCuadrados.length; i++) {
     arregloCuadrados[i].addEventListener('click', function() {
         // Toma la propiedad de backgroundcolor del cuadrado al que se le dio click
         var colorClickeado = this.style.backgroundColor;
-        if(colorClickeado == colorGanador) {
+        if(colorClickeado === colorGanador) {
             alert('auch XD');
+            mensajeDesplegar.textContent = 'Ganastee';
+        } else {
+            // Cambia el color al del body para eliminarlo y despliega Intenta otra vez
+            this.style.backgroundColor = '#232323';
+            mensajeDesplegar.textContent = 'Intenta Otra vez XD';
         }
     });
 }
+
 
