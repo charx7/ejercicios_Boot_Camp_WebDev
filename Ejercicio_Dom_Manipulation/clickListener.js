212a0a247132808a1179eb410a75636cd0cb31af
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-11-27 23:37:48 -0600

    Ejercicio de aniadir un event listener de click al objeto seleccionado con JS

---
+++ Ejercicio_Dom_Manipulation/clickListener.js
@@ -0,0 +1,6 @@
+// Selecciona el objeto al que le vamos a asignar el event listener de click
+var butonClicked = document.querySelector("button");
+// Seniala la operacion que hara el objeto y le da un click listener
+butonClicked.addEventListener("click", function(){
+    alert("AuchXD");
+});
