568d4ac6b98f0d16d7c205fd003d1a47f314671c
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-11-27 23:42:30 -0600

    Hace que el background color del html cambia dinamicamente

--- Ejercicio_Dom_Manipulation/clickListener.js
+++ Ejercicio_Dom_Manipulation/clickListener.js
@@ -3,4 +3,9 @@ var butonClicked = document.querySelector("button");
 // Seniala la operacion que hara el objeto y le da un click listener
 butonClicked.addEventListener("click", function(){
     alert("AuchXD");
+    if (document.body.style.background == "purple"){
+        document.body.style.background = "white";
+    } else {
+        document.body.style.background = "purple";
+    }
 });
