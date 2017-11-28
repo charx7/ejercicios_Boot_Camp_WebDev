ed210e190c7da86811d49cde2df7afb141009134
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-11-28 12:48:18 -0600

    Agregada funcionalidad basica y ademas lanza alerta cuando se le da click al color ganador dentro de los cuadrados

--- Ejercicio_Dom_Manipulation/GuessColorGame/funcionalidadColores.js
+++ Ejercicio_Dom_Manipulation/GuessColorGame/funcionalidadColores.js
@@ -3,7 +3,7 @@ var colores = [
     "rgb(255, 0, 0)",
     "rgb(255, 0, 0)",
     "rgb(255, 0, 0)",
-    "rgb(255, 0, 0)",
+    "rgb(255, 200, 0)",
     "rgb(255, 0, 0)",
     "rgb(255, 0, 0)"
 ];
@@ -11,8 +11,23 @@ var colores = [
 // Guardamos todos los elementos del objeto de HTML seleccionado
 var arregloCuadrados = document.querySelectorAll(".cuadrado");
 
+// Valor escogido para que sea el color a elegir como ganador
+var colorGanador = colores[3];
+// Selecciona el color para transformar el Span del html y lo procesa
+var colorDesplegado = document.getElementById("desplegadoColor");
+colorDesplegado.textContent = colorGanador;
+
+
 // Cambiamos el color de cada cuadrado de acuerdo al color del arreglo de colores
 for(var i = 0; i<arregloCuadrados.length; i++) {
     arregloCuadrados[i].style.backgroundColor = colores[i];
+    // Agregar un Event listener de cuadrado que comprueba que color eligio
+    arregloCuadrados[i].addEventListener('click', function() {
+        // Toma la propiedad de backgroundcolor del cuadrado al que se le dio click
+        var colorClickeado = this.style.backgroundColor;
+        if(colorClickeado == colorGanador) {
+            alert('auch XD');
+        }
+    });
 }
 
