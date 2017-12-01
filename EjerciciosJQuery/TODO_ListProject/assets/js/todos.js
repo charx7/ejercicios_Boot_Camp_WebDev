f1969aad756e16e51de957ffbe1e2b7ca3b34221
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-12-01 00:05:30 -0600

    Haciendo lo mismo pero con una clase de css y una solo linea de jquery

--- EjerciciosJQuery/TODO_ListProject/assets/js/todos.js
+++ EjerciciosJQuery/TODO_ListProject/assets/js/todos.js
@@ -7,20 +7,23 @@ $("li").click(function() {
 
     // Haciendolo en una sola clase
     // Verifica si el elemento li ya fue tachado antes
-    if( $(this).css("color") == "rgb(128, 128, 128)"){
-        console.log("adfadsfdsf");
-        $(this).css({
-            color: "black",
-            textDecoration: "none"
-        }); 
-    } else {
-        $(this).css({
-            color: "gray",
-            // Se tiene que usar el camel case porque el caracter - no lo soporta JS
-            textDecoration: "line-through"
-        });
-    }
+    // if( $(this).css("color") == "rgb(128, 128, 128)"){
+    //     console.log("adfadsfdsf");
+    //     $(this).css({
+    //         color: "black",
+    //         textDecoration: "none"
+    //     }); 
+    // } else {
+    //     $(this).css({
+    //         color: "gray",
+    //         // Se tiene que usar el camel case porque el caracter - no lo soporta JS
+    //         textDecoration: "line-through"
+    //     });
+    // }
+    $(this).toggleClass("completado");
 });
+
+
 
 
 
